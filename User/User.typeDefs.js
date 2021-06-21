820c2e3b627cca58634828ecabd9dd9140ff764a
구하민 <hamin2065@gmail.com>
2021-06-21 19:43:32 +0900

    followUser&unfollowUser

--- User/User.typeDefs.js
+++ User/User.typeDefs.js
@@ -10,6 +10,8 @@ export default gql`
         password: String!
         bio: String
         avatar: String
+        following: [User]
+        followers: [User]
         createdAt: String!
         updatedAt: String!
     }
---
+++ User/followUser/followUser.resolvers.js
@@ -0,0 +1,36 @@
+import client from "../../client";
+import { protectedResolver } from "../User.utils";
+
+export default {
+    Mutation: {
+        followUser:protectedResolver(async(_,args,{loggedInUser}) =>{
+            const {username} = args;
+            const check = await client.user.findUnique({
+                where:{
+                    username
+                }
+            });//follow할 유저가 존재하는지 확인
+            if(!check){
+                return{
+                    ok:false,
+                    error:"사용자가 존재하지 않습니다."
+                }
+            }//follow할 유저가 존재하지 않는 경우
+            await client.user.update({
+                where:{
+                    id:loggedInUser.id
+                },
+                data:{
+                    following:{
+                        connect:{
+                            username
+                        }
+                    }
+                }
+            });
+            return{
+                ok: true
+            }
+        })
+    }
+}
