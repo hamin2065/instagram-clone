0f78931f3beb8e907ee27b14c053980bab324c14
구하민 <hamin2065@gmail.com>
2021-09-28 14:13:35 +0900

    createAccount Error Fixed

--- User/createAccount/createAccount.typeDefs.js
+++ User/createAccount/createAccount.typeDefs.js
@@ -1,17 +1,17 @@
-import {gql} from "apollo-server";
+import { gql } from "apollo-server";
 
 export default gql`
-    type createAccountResult{
-        ok: Boolean!
-        error: String!
-    }
-    type Mutation{
-        createAccount(
-            firstName:String!
-            lastName:String
-            username:String!
-            email:String!
-            password:String!
-        ):createAccountResult
-    }  
+  type createAccountResult {
+    ok: Boolean!
+    error: String
+  }
+  type Mutation {
+    createAccount(
+      firstName: String!
+      lastName: String
+      username: String!
+      email: String!
+      password: String!
+    ): createAccountResult
+  }
 `;
